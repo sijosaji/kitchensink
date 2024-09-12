057e8e50cd9c740c8299cf3f6ad7b7e7ea4af8c4
sijosaji <sijosaji49@gmail.com>
2024-09-12 14:30:14 +0530

    Add tests

--- src/test/java/com/mongodbdemo/kitchensink/KitchensinkApplicationTests.java
+++ src/test/java/com/mongodbdemo/kitchensink/KitchensinkApplicationTests.java
@@ -1,13 +1,25 @@
 package com.mongodbdemo.kitchensink;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mockStatic;
 
 @SpringBootTest
 class KitchensinkApplicationTests {
 
 	@Test
 	void contextLoads() {
+		try (var mockedSpringApplication = mockStatic(SpringApplication.class)) {
+			mockedSpringApplication.when(() -> SpringApplication.run(any(Class.class), any(String[].class)))
+					.thenReturn(null);
+
+			KitchensinkApplication.main(new String[]{});
+
+			mockedSpringApplication.verify(() -> SpringApplication.run(KitchensinkApplication.class, new String[]{}));
+		}
 	}
 
 }
